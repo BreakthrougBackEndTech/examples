4c0e76bffc69080646021f3a2d671a3401317cc9
zhegong <zhenping.gong@nokia-sbell.com>
2019-11-03 16:52:20 +0800

    add success has sub tree

--- jianzhioffer/src/main/java/TreeType.java
+++ jianzhioffer/src/main/java/TreeType.java
@@ -153,16 +153,39 @@ public class TreeType {
    /**
     * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
     */
-   public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+   public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+       boolean result = false;
 
-       if(root2 == null){
-           return false;
+       if (root1 != null && root2 != null) {
+
+           if (!result)
+               result = isMatch(root1, root2);
+           if (!result)
+               result = HasSubtree(root1.left, root2);
+           if (!result)
+               result = HasSubtree(root1.right, root2);
        }
 
-
-       return false;
+       return result;
    }
 
+
+    private boolean isMatch(TreeNode node1, TreeNode node2) {
+
+        if (node2 == null) {
+            return true;
+        }
+
+        if (node1 == null) {
+            return false;
+        }
+
+        if (node1.val == node2.val) {
+            return isMatch(node1.left, node2.left) && isMatch(node1.right, node2.right);
+        } else {
+            return false;
+        }
+    }
 
 
     // * Definition for a binary tree node.
