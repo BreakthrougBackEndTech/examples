146179714d0cec7457a20995ceb0faa51be8d809
zhegong <zhenping.gong@nokia-sbell.com>
2019-11-04 10:10:12 +0800

    add isSymmetrical binary tree

--- jianzhioffer/src/main/java/TreeType.java
+++ jianzhioffer/src/main/java/TreeType.java
@@ -219,6 +219,31 @@ public class TreeType {
     }
 
 
+    /**
+    题目描述
+    请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的
+    */
+    public boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot == null){
+            return true;
+        }
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+
+    private boolean isSymmetrical(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return left.val == right.val && isSymmetrical(left.left, right.right) && isSymmetrical(left.right, right.left);
+    }
+
+
     // * Definition for a binary tree node.
     class TreeNode {
         int val;
