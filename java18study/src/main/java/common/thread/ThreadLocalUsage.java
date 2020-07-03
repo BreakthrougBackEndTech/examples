afe0e8ce43d0bf5fc8dacd2bd792cff332ed6590
zhegong <zhenping.gong@nokia-sbell.com>
2020-07-03 13:19:11 +0800

    add InheritableThreadLocal usage

--- java18study/src/main/java/common/thread/ThreadLocalUsage.java
+++ java18study/src/main/java/common/thread/ThreadLocalUsage.java
@@ -8,17 +8,21 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * ThreadLocal 会和线程池中的线程绑定
+ * InheritableThreadLocal  子线程会可以使用父进程的值
+ *
  * @author: zhegong
  **/
 public class ThreadLocalUsage {
     private static ThreadPoolExecutor workerThreadPool = new ThreadPoolExecutor(2, 2,
             0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(400));
 
+    public static ThreadLocal<String> parentThreadLocal = new InheritableThreadLocal<>();
 
     public static void main(String[] args) {
         workerThreadPool.setThreadFactory(new BasicThreadFactory.Builder()
                 .namingPattern("namePattern" + "-%d")
                 .build());
+        parentThreadLocal.set("main thread local vale");
         for (int i = 0; i < 10; i++) {
             Thread thread = new ThreadLocalThread("线程" + i);
             workerThreadPool.execute(thread);
@@ -49,5 +53,7 @@ class ThreadLocalThread extends Thread {
         }
         System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
         threadLocal.remove();
+        System.out.println(ThreadLocalUsage.parentThreadLocal.get());
+//        ThreadLocalUsage.parentThreadLocal.set(name + " set parent local value");
     }
 }
