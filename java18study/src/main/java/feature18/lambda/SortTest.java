89f8f48c9b93afec36e35d75eaae415ec154d879
zhegong <zhenping.gong@nokia-sbell.com>
2020-07-16 09:18:51 +0800

    add open file as stream

--- java18study/src/main/java/feature18/lambda/SortTest.java
+++ java18study/src/main/java/feature18/lambda/SortTest.java
@@ -1,6 +1,11 @@
 package feature18.lambda;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author: zhegong
@@ -51,12 +56,28 @@ public class SortTest {
         listPerson.add(p3);
 
         Collections.sort(listPerson, comparator);
+        Collections.sort(listPerson, Comparator.comparing(Person::getFirstName));
+        Collections.sort(listPerson, Comparator.comparing(Person::getFirstName).reversed()
+                .thenComparing(Person::getLastName));
+
 
         System.out.println(listPerson);
 
         Predicate<Integer> atLest5 = x -> x > 5;
 
         System.out.println(atLest5.test(5));
+
+//        IntStream.rangeClosed()
+
+        //文件打开的流
+        long uniqueWords = 0;
+        try (Stream<String> lines =
+                     Files.lines(Paths.get("data.txt"), Charset.defaultCharset())) {
+            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
+                    .distinct()
+                    .count();
+        } catch (IOException e) {
+        }
 
     }
 
@@ -73,6 +94,22 @@ public class SortTest {
         String lastName;
 
         Person() {
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
         }
 
         Person(String firstName, String lastName) {
